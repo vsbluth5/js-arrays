05b88c2e2a6928e25b90d522b80d679209732b32
Glitch (upperlinecode-js-array-map-iteration-practice-lab) <none>
2020-06-18 03:02:08 +0000

    Import of upperlinecode/js-array-map-iteration-practice-lab

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Get Started!
+# JavaScript Iteration Practice
 
-Add files and start typing 🎉
+This lab includes an opportunity to iterate with `for` loops. All the tasks in Part A will require a `for` loop.
+
+JavaScript also includes some built-in methods for array iteration, and some of the most useful are:
+* `.map()`, which iterates and returns a parallel copy of the array, usually modified in some way.
+* `.filter()`, which iterates and returns a subset of the values in the original array.
+* `.forEach()`, which iterates and returns nothing.
+
+Feel free to do all the Part B tasks with a traditional `for` loop. If you want a challenge, you can try to use `.map()`, and for an even more significant challenge, try one of the others.
+
+Part C includes only challenges. 
---
+++ script.js
@@ -0,0 +1,82 @@
+console.log("Script Running!")
+
+//// Part A: For Loops
+
+//// SAMPLE CODE
+// for (let i=0; i<11; i++){
+//   console.log(i)
+// }
+
+//// 1: Console log out every number from 1-100
+
+
+//// 2: Log out every number from 50-100
+
+
+//// 3: Log out every even number from 1-100
+
+
+//// 4 (CHALLENGE): Log out every square number from 1-225
+
+
+//// 5: Log out every multiple of 3 from 1-100
+
+
+
+
+//// PART B: Array Iteration
+
+//// 6. Here's an array of all of Alex's favorite foods. For each food in the array, log out a statement that says "I really love _____" and then fill in the blank with each.
+// favorite_foods = ["Sushi", "Pizza", "Salmon", "Tamales dulces", "Yuca fries", "Apple pie", "Katsu curry", "Shrimp and grits", "Waffles"]
+
+
+
+//// 7. Now for each food in that array, let's be specific about how MUCH Alex likes them.
+////    For example, log out "Alex's #1 favorite food is Sushi" and then "Alex's #2 favorite food is Pizza" and so on until you've made an ordered list.
+////    This can be done a number of different ways, but the easiest is probably by iterating over each index in the favorite_foods array using a function called "range". Try to find it in the documentation.
+
+
+
+
+//// 8. Below is a massive array of numbers. Iterate over the array and log out only the numbers less than 100. 
+// many_numbers = [109, 141, 44, 51, 133, 366, 339, 248, 226, 321, 97, 195, 245, 252, 238, 1, 366, 47, 189, 91, 148, 88, 194, 106, 5, 128, 165, 337, 380, 181, 143, 95]
+
+
+
+//// 9. Now iterate over that same list, but log out only the even ones. 
+
+
+
+//// 10. Below is a array of 99 names. Iterate over them all and only log out names with more than 7 characters.
+// many_names = ["Alexa","Burke","Kasimir","Baxter","Carissa","Vielka","Derek","Jemima","Jackson","Keegan","Graham","Melissa","Jeanette","Grant","Kirsten","Naida","Brody","Ishmael","Kane","Seth","Rae","Eagan","Camille","Alana",
+//   "Vance","Melinda","Tarik","Risa","Jordan","Camilla","Karly","Baker","Adena","Calvin","Kendall","Nasim","Kellie","Dana","Rhoda","Linus","Tyler","Ahmed","Dante","Shay","Lael","Tana","Claudia","Chadwick","Tara",
+//   "Fulton","Justine","Malcolm","Rowan","Christopher","Ciaran","Ivan","Hiram","Blake","Colton","Nathaniel","Moses","Cynthia","George","Ignacia","Chanda","Wyatt","Amethyst","Vladimir","Adam","Boris","Joseph","Scarlett","Kieran","Curran",
+//   "Dalton","Paul","Phillip","Plato","Renee","Natalie","Barbara","Keiko","Oleg","Xerxes","Caesar","Kareem","Ahmed","Charles","Cyrus","Adria","Winifred","Pandora","Wynne","Simon","Wanda","Coby","Nolan","Marsden","Courtney"]
+
+
+
+
+//// 11. Iterate over the many_names array and find the longest name in the whole thing.
+////    Then print out "The longest name in the array is ______. It has ___ letters in it."
+
+
+
+
+//// 12. Take that same array and sort it into two arrays: one array of long names, which have more than 6 characters, and one array of short names which have 6 or fewer characters. Then print out both arrays.
+
+
+
+
+
+//// Part C: CHALLENGES:
+
+//// 13 (CHALLENGE): Log out every number from 1-100 which is a multiple of 3 AND an even number.
+
+
+//// 14 (Challenge): Log out the first 40 numbers in the fibbonacci sequence
+
+
+//// 15 (Challenge): Log out the sum of every integer from 1-1000
+
+
+//// 16 (Challenge): In the super long list of names, figure out if there are any doubles.
