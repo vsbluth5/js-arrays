ec6b0dce49e3d10752e364dafea9ecb69626b927
Glitch (upperlinecode-js-array-map-iteration-practice-lab) <none>
2020-06-19 02:12:10 +0000

    🗽🌋 Checkpoint ./notes.js:3856783/467

---
+++ notes.js
@@ -0,0 +1,25 @@
+/*
+First idea:
+nameOfList.forEach(myfunction)
+which is the same as
+
+nameOfList.forEach(listItem => {
+  code to execute for each listItem in nameOfList
+})
+
+
+Second idea:
+for (let i = 0; i < nameOfList.length; i++) {
+ code which does something with nameOfList[i]
+}
+
+Can also use for-loop in gnereal:
+for (int i = start; test which returns true until finished; change i to get closer to finish) {
+  code which inlcudes i or not
+}
+
+
+
+
+
+*/
